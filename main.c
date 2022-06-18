88b492e384d2f2d183d0be9f7dfa443eb3890dd1
fox0 <red-fox0@yandex.ru>
2022-06-18 21:27:21 +0700

    up

--- main.c
+++ main.c
@@ -5,27 +5,37 @@
 #define SIZEOF_VOID_P 8
 
 #include <stdarg.h>
-#include <WinCryptEx.h>
-#include <cades.h>
-
-#include <stdio.h>
 #include <string.h>
+//#include <stdio.h>
 #include <assert.h>
+#include <WinCryptEx.h>
 
 
-// /opt/cprocsp/bin/amd64/certmgr certmgr --list --thumbprint 046255290b0eb1cdd1797d9ab8c81f699e3687f3
-int main() {
-    const char *store = "MY";
-    const char *thumbprint = "046255290b0eb1cdd1797d9ab8c81f699e3687f3";
+void *open_store(const char *store_name) {
+    if (strlen(store_name) == 0) {
+        return NULL;
+    }
+    return CertOpenSystemStoreA(0, store_name);
+}
 
-    int r;
+int close_store(void *store) {
+    if (!store) {
+        return 0;
+    }
+    return CertCloseStore(store, 0);
+}
 
-    void *cert_store = CertOpenSystemStoreA(0, store);
-    assert(cert_store);
+void *find_certificate_by_thumbprint(void *store, const char *thumbprint) {
+    if (!store) {
+        return NULL;
+    }
+    if (strlen(thumbprint) != 40) {
+        return NULL;
+    }
 
     unsigned int hash_len = 20;
     unsigned char hash[hash_len];
-    r = CryptStringToBinaryA(
+    int r = CryptStringToBinaryA(
             thumbprint,
             strlen(thumbprint),
             CRYPT_STRING_HEX,
@@ -34,23 +44,37 @@ int main() {
             NULL,
             NULL
     );
-    assert(r);
+    if (r == 0) {
+        return NULL;
+    }
 
-    CRYPT_INTEGER_BLOB para = {
-            .cbData = hash_len,
-            .pbData = hash
-    };
-
-    PCCERT_CONTEXT cert_context = CertFindCertificateInStore(
-            cert_store,
+    DATA_BLOB para = {.cbData = hash_len, .pbData = hash};
+    PCCERT_CONTEXT result = CertFindCertificateInStore(
+            store,
             PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
             0,
             CERT_FIND_HASH,
             &para,
             NULL
     );
+    return (void *) result;
+}
+
+
+// /opt/cprocsp/bin/amd64/certmgr certmgr --list --thumbprint 046255290b0eb1cdd1797d9ab8c81f699e3687f3
+int main() {
+    const char *store_name = "MY";
+    const char *thumbprint = "046255290b0eb1cdd1797d9ab8c81f699e3687f3";
+
+    int r;
+
+    void *store = open_store(store_name);
+    assert(store);
+
+    void *cert_context = find_certificate_by_thumbprint(store, thumbprint);
     assert(cert_context);
 
-    r = CertCloseStore(cert_store, 0);
+
+    r = close_store(store);
     assert(r);
 }
